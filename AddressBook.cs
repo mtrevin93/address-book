650c54e573f12161e42dbcbc79df42e36520e7f6
Michael Trevino <mtrevin93@gmail.com>
2021-08-27 14:21:58 -0400

    added try/catch for addContact error

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,15 +1,23 @@
 using System.Collections.Generic;
+using System;
 namespace address_book
 {
 public class AddressBook{
 public Dictionary<string, Contact> ContactInfo { get; set; }
 public AddressBook()
 {
-    // ContactInfo = new Dictionary<string, Contact>();
+    ContactInfo = new Dictionary<string, Contact>();
 }
 public void AddContact(Contact contact)
 {
+    try
+    {
     this.ContactInfo.Add(contact.Email, contact);
+    }
+    catch (System.ArgumentException ex)
+    {
+        Console.WriteLine($"Error trying to add contact: {ex}\n");
+    }
 }
 public Contact GetByEmail(string email)
 {
